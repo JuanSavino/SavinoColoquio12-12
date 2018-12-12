534b22d88133b8819d79a2d9524c882a59d55db4
Juan Cruz <juancruz.savino431@gmail.com>
2018-12-12 09:42:36 -0300

    Add files via upload

--- app/models.py
+++ app/models.py
@@ -1,6 +1,16 @@
-# -*- coding: utf-8 -*-
 from __future__ import unicode_literals
-
+from django.conf import settings
 from django.db import models
 
+
 # Create your models here.
+
+
+
+class Alumno(models.Model):
+    name = models.CharField(max_length=50)
+
+
+class Curso(models.Model):
+    name = models.CharField(max_length=50)
+
--- app/views.py
+++ app/views.py
@@ -1,6 +1,14 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 
+from app.models import Alumno
+
 from django.shortcuts import render
 
 # Create your views here.
+
+def dame_nombres(request):
+    context = {}
+    context['Alumnos'] = Alumno.objects.all()
+    return render(request, 'index.html', context)
+
